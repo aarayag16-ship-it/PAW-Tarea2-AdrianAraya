377fb172600cedd6fcd413a9af53f646b9b0ae8e
Adrian Araya <aarayag16@gmail.com>
2025-10-04 17:44:15 -0600

    Prueba 2

---
+++ Tarea_2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tarea_2.Startup))]
+namespace Tarea_2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
